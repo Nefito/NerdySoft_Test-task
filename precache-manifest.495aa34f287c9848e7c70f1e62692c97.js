9fe9ece7ffea12d0fea4f396828895e1395b7e79
Nefito <vkorzhov00@gmail.com>
2020-08-26 00:01:33 +0300

    Updates

---
+++ precache-manifest.495aa34f287c9848e7c70f1e62692c97.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e9da4a4153e3df0294ed13e7fe1642",
+    "url": "/NerdySoft_Test-Task/index.html"
+  },
+  {
+    "revision": "ec2812670f4915daa7ff",
+    "url": "/NerdySoft_Test-Task/static/css/2.f636f832.chunk.css"
+  },
+  {
+    "revision": "80339abac52bb7d88149",
+    "url": "/NerdySoft_Test-Task/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ec2812670f4915daa7ff",
+    "url": "/NerdySoft_Test-Task/static/js/2.0c93f683.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/NerdySoft_Test-Task/static/js/2.0c93f683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80339abac52bb7d88149",
+    "url": "/NerdySoft_Test-Task/static/js/main.5a3f7231.chunk.js"
+  },
+  {
+    "revision": "92e3468879739df91730",
+    "url": "/NerdySoft_Test-Task/static/js/runtime-main.f33331cf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/NerdySoft_Test-Task/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/NerdySoft_Test-Task/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/NerdySoft_Test-Task/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/NerdySoft_Test-Task/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/NerdySoft_Test-Task/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
