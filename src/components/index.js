510154f307ca008add251dbbd7d81895d176cf62
Nefito <vkorzhov00@gmail.com>
2020-09-22 13:48:54 +0300

    Create index.js file in components folder

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as MyModal} from './ModalComponent.jsx';
+export {default as RenderAnnouncement} from './RenderAnnouncementComponent.jsx';
+export {default as Home} from './HomeComponent.jsx';
+export {default as Main} from './MainComponent.jsx';
