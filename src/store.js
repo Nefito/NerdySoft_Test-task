c4e2fb25e9906fcf636f22636f032fa1bf6829d0
Nefito <vkorzhov00@gmail.com>
2020-09-23 18:03:56 +0300

    remove useless file

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import announcementsReducer from './Announcement/AnnouncementSlice';
-
-export default configureStore({
-  reducer: {
-      announcements: announcementsReducer
-  }
-});
