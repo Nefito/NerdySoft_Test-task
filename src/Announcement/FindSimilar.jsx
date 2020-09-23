bbe0eabf9e08f8bd0100b07f18e18ac20d372fa2
Nefito <vkorzhov00@gmail.com>
2020-09-23 10:42:50 +0300

    Combine FindSimilar and RenderSimilar into 1 file

--- src/Announcement/FindSimilar.jsx
+++ src/Announcement/FindSimilar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import RenderSimilarAnn from './RenderSimilar';
+import {RenderAnnouncement} from '../components';
 
 const FindSimilar = ({announcements, selectedAnn}) => {
     let all_words_count = new Map();
@@ -26,11 +26,26 @@ const FindSimilar = ({announcements, selectedAnn}) => {
             all_words_count.set(ann.ID, title_word_count+desc_words_count);
         }
     });
-    return (
-        <div>
-            <RenderSimilarAnn announcements={announcements} all_words_count={all_words_count}/>
-        </div>
-    );
+
+    let annID_list = [];
+    let sorted_map = new Map([...all_words_count.entries()].sort((a, b) => b[1] - a[1]));
+    for (let i = 0; i < 3; i++) {
+        for(let [key, value] of sorted_map.entries()) {
+            if(value > 1){
+                annID_list.push(key);
+                sorted_map.delete(key);
+            }
+        }
+        if (annID_list.length > 0) {
+            return annID_list.map((ID) => {
+                const ann = announcements.find(ann => ann.ID === ID);
+                return (
+                    <RenderAnnouncement ann={ann} divClass="mt-3" cardClass="text-center mt-2" fullText={false} />
+                );
+            });
+        }
+        return (<h4> No Similar Announcements Were Found :(</h4>);
+    }
 }
 
 export default FindSimilar;
